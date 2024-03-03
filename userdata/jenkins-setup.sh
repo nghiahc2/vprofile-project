2a9b49bf016f00493a027693ea94d10cdd4cb6aa
nghiahc2 <nghiahc2@gmail.com>
2024-03-03 10:03:14 +0700

    Update jenkins-setup.sh

--- userdata/jenkins-setup.sh
+++ userdata/jenkins-setup.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+
+sudo yum update –y
+sudo wget -O /etc/yum.repos.d/jenkins.repo \
+    https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+sudo yum upgrade
+sudo dnf install java-17-amazon-corretto -y
+sudo yum install git maven jenkins -y
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
+
+######################################3
 sudo apt update
 sudo apt install openjdk-11-jdk -y
 sudo apt install maven wget unzip -y
